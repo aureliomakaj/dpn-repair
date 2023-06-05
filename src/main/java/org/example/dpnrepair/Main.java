544e72fed8aa1798edafc2c7673ad169de360789
Aurelio Makaj <aurelio.makaj@electrolux.com>
2023-06-05 21:14:06 +0200

    Other

--- src/main/java/org/example/dpnrepair/Main.java
+++ src/main/java/org/example/dpnrepair/Main.java
@@ -21,20 +21,21 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, DPNParserException, TransformerException {
 
-        String figure = "src/main/resources/figure1-dpn.pnml";
+//        String figure = "src/main/resources/figure1-dpn.pnml";
 //        String figure = "src/main/resources/figure-2-dpn.pnml";
 //        String figure = "src/main/resources/figure-3-dpn.pnml";
-//        String figure = "src/main/resources/figure-4-dpn.pnml";
+        String figure = "src/main/resources/figure-4-dpn.pnml";
         DPNParser parser = new DPNParser(figure);
         parser.parse();
+        System.out.println("HAS CYCLES: " + DPNUtils.hasCycles(parser.getDpn()));
 //        ConstraintGraph cg = new ConstraintGraph(parser.getDpn());
 //        ConstraintGraphPrinter cgPrinter = new ConstraintGraphPrinter(cg);
 //        cgPrinter.writeRaw("figure-3-cg.txt");
 //        DPNRepairAcyclic dpnRepairAcyclic = new DPNRepairAcyclic(parser.getDpn());
 //        dpnRepairAcyclic.repair();
-        DPNRepairCyclic dpnRepairCyclic = new DPNRepairCyclic(parser.getDpn());
-        dpnRepairCyclic.repair();
-        ConstraintGraph cg2 = new ConstraintGraph(dpnRepairCyclic.getRepaired());
+//        DPNRepairCyclic dpnRepairCyclic = new DPNRepairCyclic(parser.getDpn());
+//        dpnRepairCyclic.repair();
+//        ConstraintGraph cg2 = new ConstraintGraph(dpnRepairCyclic.getRepaired());
         System.out.println("Finished");
     }
 }
