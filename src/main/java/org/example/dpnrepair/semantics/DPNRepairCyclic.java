f8f1eff8da2660de4cc2ad4e2ae7321b5d0ed360
Aurelio Makaj <aurelio.makaj@electrolux.com>
2023-06-05 21:13:42 +0200

    Fix algorithm 3

--- src/main/java/org/example/dpnrepair/semantics/DPNRepairCyclic.java
+++ src/main/java/org/example/dpnrepair/semantics/DPNRepairCyclic.java
@@ -15,33 +15,34 @@ public class DPNRepairCyclic extends DPNRepairAcyclic {
 
         Constraint guard = t.getGuard();
         Constraint guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getFirst(), guard.getSecond(), c);
-        if (guardUnderlyingNet.isStrict() && guardUnderlyingNet.getValue() == 0) {
+        if ((guardUnderlyingNet.isStrict() && guardUnderlyingNet.getValue() == 0) || guard.getFirst().equals(Constraint.ZED) || guard.getSecond().equals(Constraint.ZED)) {
             Transition t1 = firstCopy.dpn.getTransitions().get(t.getId());
             t1.setGuard(guardUnderlyingNet.clone());
-            if (guard.isStrict() != guardUnderlyingNet.isStrict() && guard.getValue() != guardUnderlyingNet.getValue() ) {
+            if (guard.isStrict() != guardUnderlyingNet.isStrict() || guard.getValue() != guardUnderlyingNet.getValue()) {
                 firstCopy.modifiedTransitions.add(t.getId());
                 firstCopy.changes++;
             }
             updatePriorityQueue(firstCopy);
+        } else {
+            RepairDPN secondCopy = makeCopy(net);
+            guard = t.getGuard();
+            guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getFirst(), Constraint.ZED, c);
+            Transition t2 = secondCopy.dpn.getTransitions().get(t.getId());
+            t2.setGuard(guardUnderlyingNet.clone());
+            secondCopy.modifiedTransitions.add(t.getId());
+            secondCopy.changes++;
+            updatePriorityQueue(secondCopy);
+
+            RepairDPN thirdCopy = makeCopy(net);
+            guard = t.getGuard();
+            guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(Constraint.ZED, guard.getSecond(), c);
+            Transition t3 = thirdCopy.dpn.getTransitions().get(t.getId());
+            t3.setGuard(guardUnderlyingNet.clone());
+            thirdCopy.modifiedTransitions.add(t.getId());
+            thirdCopy.changes++;
+            updatePriorityQueue(thirdCopy);
         }
 
-        RepairDPN secondCopy = makeCopy(net);
-        guard = t.getGuard();
-        guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getFirst(), Constraint.ZED, c);
-        Transition t2 = secondCopy.dpn.getTransitions().get(t.getId());
-        t2.setGuard(guardUnderlyingNet.clone());
-        secondCopy.modifiedTransitions.add(t.getId());
-        secondCopy.changes++;
-        updatePriorityQueue(secondCopy);
-
-        RepairDPN thirdCopy = makeCopy(net);
-        guard = t.getGuard();
-        guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(Constraint.ZED, guard.getSecond(), c);
-        Transition t3 = thirdCopy.dpn.getTransitions().get(t.getId());
-        t3.setGuard(guardUnderlyingNet.clone());
-        thirdCopy.modifiedTransitions.add(t.getId());
-        thirdCopy.changes++;
-        updatePriorityQueue(thirdCopy);
     }
 
     protected void backwardRepair(RepairDPN net, Transition t, DifferenceConstraintSet c) {
@@ -49,7 +50,7 @@ public class DPNRepairCyclic extends DPNRepairAcyclic {
 
         Constraint guard = t.getGuard();
         Constraint guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getSecond(), guard.getFirst(), c);
-        if (guardUnderlyingNet.getValue() == 0) {
+        if ((guardUnderlyingNet.getValue() == 0) || guard.getFirst().equals(Constraint.ZED) || guard.getSecond().equals(Constraint.ZED)) {
             Constraint guardCopy = guard.clone();
             guardCopy.setStrict(!guardUnderlyingNet.isStrict()); // Switch strictness
             Transition t2 = firstCopy.dpn.getTransitions().get(t.getId());
@@ -57,34 +58,35 @@ public class DPNRepairCyclic extends DPNRepairAcyclic {
             firstCopy.modifiedTransitions.add(t.getId());
             firstCopy.changes++;
             updatePriorityQueue(firstCopy);
+        } else {
+            RepairDPN secondCopy = makeCopy(net);
+            Constraint guardUnderlyingNet2 = getConstraintFromDifferenceConstraintSet(guard.getSecond(), Constraint.ZED, c);
+            if (guardUnderlyingNet2.getValue() != Long.MAX_VALUE) {
+                Constraint guardCopy = guard.clone();
+                guardCopy.setFirst(Constraint.ZED);
+                guardCopy.setStrict(!guardUnderlyingNet2.isStrict()); // Switch strictness
+                Transition t2 = secondCopy.dpn.getTransitions().get(t.getId());
+                t2.setGuard(guardCopy);
+                secondCopy.modifiedTransitions.add(t.getId());
+                secondCopy.changes++;
+                updatePriorityQueue(secondCopy);
+            }
+
+
+            RepairDPN thirdCopy = makeCopy(net);
+            Constraint guardUnderlyingNet3 = getConstraintFromDifferenceConstraintSet(Constraint.ZED, guard.getFirst(), c);
+            if (guardUnderlyingNet3.getValue() != Long.MAX_VALUE) {
+                Constraint guardCopy = guard.clone();
+                guardCopy.setSecond(Constraint.ZED);
+                guardCopy.setStrict(!guardUnderlyingNet3.isStrict()); // Switch strictness
+                Transition t2 = thirdCopy.dpn.getTransitions().get(t.getId());
+                t2.setGuard(guardCopy);
+                thirdCopy.modifiedTransitions.add(t.getId());
+                thirdCopy.changes++;
+                updatePriorityQueue(thirdCopy);
+            }
         }
 
-        RepairDPN secondCopy = makeCopy(net);
-        Constraint guardUnderlyingNet2 = getConstraintFromDifferenceConstraintSet(guard.getSecond(), Constraint.ZED, c);
-        if (guardUnderlyingNet2.getValue() != Long.MAX_VALUE) {
-            Constraint guardCopy = guard.clone();
-            guardCopy.setFirst(Constraint.ZED);
-            guardCopy.setStrict(!guardUnderlyingNet2.isStrict()); // Switch strictness
-            Transition t2 = secondCopy.dpn.getTransitions().get(t.getId());
-            t2.setGuard(guardCopy);
-            secondCopy.modifiedTransitions.add(t.getId());
-            secondCopy.changes++;
-            updatePriorityQueue(secondCopy);
-        }
-
-
-        RepairDPN thirdCopy = makeCopy(net);
-        Constraint guardUnderlyingNet3 = getConstraintFromDifferenceConstraintSet(Constraint.ZED, guard.getFirst(), c);
-        if (guardUnderlyingNet3.getValue() != Long.MAX_VALUE) {
-            Constraint guardCopy = guard.clone();
-            guardCopy.setSecond(Constraint.ZED);
-            guardCopy.setStrict(!guardUnderlyingNet3.isStrict()); // Switch strictness
-            Transition t2 = thirdCopy.dpn.getTransitions().get(t.getId());
-            t2.setGuard(guardCopy);
-            thirdCopy.modifiedTransitions.add(t.getId());
-            thirdCopy.changes++;
-            updatePriorityQueue(thirdCopy);
-        }
     }
 
 }
