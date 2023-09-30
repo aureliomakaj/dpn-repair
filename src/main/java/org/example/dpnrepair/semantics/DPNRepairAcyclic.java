a179d7393c995bd369f3fdba2a7d8efa96c12dc7
aureliomakaj <makaj.aurelio@gmail.com>
2023-09-30 10:07:21 +0200

    Fix comments

--- src/main/java/org/example/dpnrepair/semantics/DPNRepairAcyclic.java
+++ src/main/java/org/example/dpnrepair/semantics/DPNRepairAcyclic.java
@@ -75,7 +75,7 @@ public class DPNRepairAcyclic {
             for (Transition enabledTransition : enabledTransitions) {
                 forwardRepair(net, enabledTransition, curr.getCanonicalForm());
             }
-            // Let BW be the set of transitions in all paths (M0,C0) ⇝ (M,C).
+            // Let BW be the set of transitions in all paths (M0,C0) -> (M,C).
             Set<Transition> previousTransitions = DPNUtils.getPreviousTransitions(net.dpn, cg, curr, false);
             for (Transition t : previousTransitions) {
                 backwardRepair(net, t, curr.getCanonicalForm());
@@ -88,12 +88,12 @@ public class DPNRepairAcyclic {
         // Let N′′ := (P, T, F, V, αI , guard′′) be a copy of N′.
         RepairDPN copy = makeCopy(net);
 
-        // Let y − x ▷◁ k be the guard of t.
+        // Let y − x op k be the guard of t.
         Constraint guard = t.getGuard();
-        // Let y − x ▷◁′ k′ be the corresponding constraint in C.
+        // Let y − x op′ k′ be the corresponding constraint in C.
         Constraint guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getFirst(), guard.getSecond(), c);
         Transition t2 = copy.dpn.getTransitions().get(t.getId());
-        // guard′′(t) := y − x ▷◁′ k′
+        // guard′′(t) := y − x op′ k′
         t2.setGuard(guardUnderlyingNet.clone());
         copy.modifiedTransitions.add(t.getId());
         copy.changes++;
@@ -114,18 +114,19 @@ public class DPNRepairAcyclic {
                 .filter(constr -> first.equals(constr.getFirst()) && second.equals(constr.getSecond()))
                 .findFirst();
 
-        return constraint.orElse(new Constraint(first, second));
+        return constraint
+                .orElse(new Constraint(first, second));
     }
 
-    // “replace with the opposite constraint of C”
+    // “replace with the opposite constraint of C''
     protected void backwardRepair(RepairDPN net, Transition t, DifferenceConstraintSet c) {
         // Let N′′ := (P, T, F, V, αI , guard′) be a copy of N′.
         RepairDPN copy = makeCopy(net);
-        // Let y − x ▷◁ k be the guard of t.
+        // Let y − x op k be the guard of t.
         Constraint guard = t.getGuard();
-        // x − y ▷◁′ k′ in C
+        // x − y op′ k′ in C
         Constraint guardUnderlyingNet = getConstraintFromDifferenceConstraintSet(guard.getSecond(), guard.getFirst(), c);
-        // if x − y ▷◁′ k′ in C is such that k′ != +Inf then
+        // if x − y op′ k′ in C is such that k′ != +Inf then
         if (guardUnderlyingNet.getValue() != Long.MAX_VALUE) {
             Constraint guardCopy = guard.clone();
             guardCopy.setValue(guardUnderlyingNet.getNegatedValue()); // Negate value
@@ -149,7 +150,7 @@ public class DPNRepairAcyclic {
             List<ConstraintGraph.Node> nodes = getFiring(cg, missingTransition);
             for (ConstraintGraph.Node node : nodes) {
                 forwardRepair(net, missingTransition, node.getCanonicalForm());
-                // Let BW be the set of non-silent transitions in all paths (M0,C0) ⇝ (M,C).
+                // Let BW be the set of non-silent transitions in all paths (M0,C0) -> (M,C).
                 Set<Transition> previousTransitions = DPNUtils.getPreviousTransitions(net.dpn, cg, node, true);
                 for (Transition previousTransition : previousTransitions) {
                     backForwardRepair(net, previousTransition);
@@ -177,12 +178,12 @@ public class DPNRepairAcyclic {
                 .collect(Collectors.toList());
     }
 
-    // “make the guard true”
+    // “make the guard true''
     protected void backForwardRepair(RepairDPN net, Transition t) {
         // Let N′′ := (P, T, F, V, αI , guard′) be a copy of N′.
         RepairDPN copy = makeCopy(net);
 
-        // Let y − x ▷◁ k be the guard of t
+        // Let y − x op k be the guard of t
         Transition t2 = copy.dpn.getTransitions().get(t.getId());
         // guard′′(t) := y − x ≤ +Inf
         Constraint newGuard = t2.getGuard().clone();
