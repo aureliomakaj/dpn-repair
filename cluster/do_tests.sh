c63197704f2423696fa75f1f0476c2f4d3aae2e2
Davide <bresolin.davide@gmail.com>
2024-01-09 14:50:32 +0100

    New jar and do_test script

--- cluster/do_tests.sh
+++ cluster/do_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-for f in *.pnml; do
+for f in acyclic/*.pnml; do
 	echo Running ${f} ...
-	java -jar DPNRepair-fat-1.0-SNAPSHOT.jar ${f}
+	java -Xmx32G -jar DPNRepair-fat-1.0-SNAPSHOT.jar --file-path ${f} 
+done
+
+for f in cyclic/*.pnml; do
+	echo Running ${f} ...
+	java -Xmx32G -jar DPNRepair-fat-1.0-SNAPSHOT.jar --cyclic --file-path ${f} 
 done
